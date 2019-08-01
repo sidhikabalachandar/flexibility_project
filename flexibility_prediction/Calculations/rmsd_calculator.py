7b184004ecd177162fd9105604ba252283bd3f8e
Sidhika Balachandar <sidhikab@sh-ln06.stanford.edu>
2019-08-01 11:04:24 -0700

    All protein flexibility prediction rmsds

--- flexibility_prediction/Calculations/rmsd_calculator.py
+++ flexibility_prediction/Calculations/rmsd_calculator.py
@@ -7,24 +7,15 @@ Only the residues within 4 angstroms of either structures' ligands are considere
 # ml load schrodinger
 # $SCHRODINGER/run python3 rmsd_calculator.py
 
-from schrodinger.structure import StructureReader
+import schrodinger.structure as structure
 import schrodinger.structutils.measure as measure
 import schrodinger.structutils.rmsd as rmsd
 import schrodinger.structutils.analyze as analyze
 import os
 import pickle
 import csv
-
-
-#Loop over each protein
-#Loop over each starting ligand and structure
-#Loop over each target ligand and structure
-#Get the alignment string between these two structures
-#Get all the residues in each structure
-#Create a map of residues to alignment string index
-#Create a map of alignment string index to residues
-#Get all residues near the ligand
-#Reduce to only the ligands that are paired between each structure
+import sys
+import time
 
 
 '''
@@ -56,6 +47,7 @@ def map_residues_to_align_index(alignment_string, r_list):
 		if counter >= len(r_list):
 			break
 		if alignment_string[i] == r_list[counter][0]:
+			#print(r_list[counter], i)
 			r_to_i_map[r_list[counter]] = i
 			counter += 1
 	return r_to_i_map
@@ -72,13 +64,23 @@ def inv_map(m):
 '''
 This function gets the unique identifier for all residues within 4 angstroms of the ligand
 '''
-def get_res_near_ligand(s):
+def get_res_near_ligand(s, r_to_i_map):
 	cutoff = 4
-	l = analyze.find_ligands(s)[0]
+	ligand_alist = []
+	for m in list(s.molecule):
+		for r in list(m.residue):
+			if list(r.atom)[0].chain == "L":
+				ligand_alist += r.getAtomList()
+	if ligand_alist == []:
+		return 0
 	#get atom indexes for the ligand
-	close_a_indices = measure.get_atoms_close_to_subset(s, l.atom_indexes, cutoff)
-	close_r_set = set([(s.atom[i].pdbcode, s.atom[i].chain, s.atom[i].resnum, s.atom[i].inscode) for i in close_a_indices])
+	close_a_indices = measure.get_atoms_close_to_subset(s, ligand_alist, cutoff)
+	close_r_set = set({})
+	for i in close_a_indices:
+		if (s.atom[i].pdbcode, s.atom[i].chain, s.atom[i].resnum, s.atom[i].inscode) in r_to_i_map:
+			close_r_set.add((s.atom[i].pdbcode, s.atom[i].chain, s.atom[i].resnum, s.atom[i].inscode))
 	return close_r_set
+
 
 
 '''
@@ -97,81 +99,136 @@ def get_atoms(s, final_r_list):
 	return (asl_list, complete_a_list)
 
 
-if __name__ == '__main__':
-	folder = "/scratch/PI/rondror/combind/bpp_data/"
-	#proteins = os.listdir(folder)
-	proteins = ['MAPK14']
-	protein_dict = {}
-	infile = open('MAPK14_ASL_to_resinfo_dict', 'rb')
-	ASL_to_feature = pickle.load(infile)
-	infile.close()
+def get_ligands(protein, max_ligands, combind_root):
+    ligand_folder = combind_root + "/" + protein + "/docking/grids"
+    ligands = sorted(os.listdir(ligand_folder))[:max_ligands]  # sorted
+    return ligands
 
 
-	with open('res_flexibility.csv', 'w') as csvFile:
+def get_proteins(combind_root):
+	'''
+	Get the list of all proteins
+	:param combind_root: path to the combind root folder
+	:return: list of protein name strings
+	'''
+	proteins = sorted(os.listdir(combind_root))
+	proteins = [p for p in proteins if p[0] != '.']
+	print(proteins)
+	return proteins
+
+
+def compute_protein_rmsds(protein, rmsd_file, ASL_to_feature, combind_root):
+	max_ligands = 25
+
+
+	with open(rmsd_file, 'w') as csvFile:
 		writer = csv.writer(csvFile)
 		writer.writerow(['protein', 'start ligand', 'target ligand', 'rmsd', 'bfactor', 'normalized bfactor', 'normal variate bfactor', 'res name', 'secondary structure'])
 
-		for protein in proteins:
-			if protein[0] != '.':
-				ligand_file = folder + protein + "/structures/aligned_files/"
-				ligands = os.listdir(ligand_file)
-				ligands.remove("4DLI")
-				infile = open('MAPK14_pairwise_alignment','rb')
-				paired_strs = pickle.load(infile)
-				infile.close()
-				rmsds = []
+		ligands = get_ligands(protein, max_ligands, combind_root)
+		infile = open('../../protein_flexibility/Data/alignments/{}_alignment.pkl'.format(protein),'rb')
+		paired_strs = pickle.load(infile)
+		infile.close()
 
-				for i, start in enumerate(ligands):
-					ending_1 = '{}/structures/aligned_files/{}/{}_out.mae'.format(protein, start, start)
-					s1 = list(StructureReader(folder + ending_1))[0]
+		for start in ligands:
 
-					arr = []
+			if start not in ASL_to_feature[protein]:
+				print(protein, start, "not in dict")
+				#continue
+			ending_1 = '{}/structures/aligned_files/{}/{}_out.mae'.format(protein, start, start)
+			s1 = list(structure.StructureReader(combind_root + ending_1))[0]
 
-					for j, target in enumerate(ligands):
+			for target in ligands:
 
-						if i != j:
+				if start != target:
+					ending_2 = '{}/structures/aligned_files/{}/{}_out.mae'.format(protein, target, target)
+					s2 = list(structure.StructureReader(combind_root + ending_2))[0]
 
-							ending_2 = '{}/structures/aligned_files/{}/{}_out.mae'.format(protein, target, target)
-							s2 = list(StructureReader(folder + ending_2))[0]
+					if start < target:
+						(paired_str_s1, paired_str_s2) = paired_strs[start][target]
+					else:
+						(paired_str_s2, paired_str_s1) = paired_strs[target][start]
 
-							(paired_str_s1, paired_str_s2) = paired_strs[i][j]
+					r_list_s1 = get_all_res(s1)
+					r_list_s2 = get_all_res(s2)
 
-							r_list_s1 = get_all_res(s1)
-							r_list_s2 = get_all_res(s2)
+					r_to_i_map_s1 = map_residues_to_align_index(paired_str_s1, r_list_s1)
+					r_to_i_map_s2 = map_residues_to_align_index(paired_str_s2, r_list_s2)
+					i_to_r_map_s1 = inv_map(r_to_i_map_s1)
+					i_to_r_map_s2 = inv_map(r_to_i_map_s2)
 
-							r_to_i_map_s1 = map_residues_to_align_index(paired_str_s1, r_list_s1)
-							r_to_i_map_s2 = map_residues_to_align_index(paired_str_s2, r_list_s2)
-							i_to_r_map_s1 = inv_map(r_to_i_map_s1)
-							i_to_r_map_s2 = inv_map(r_to_i_map_s2)
+					valid_r_s1 = get_res_near_ligand(s1, r_to_i_map_s1)
+					valid_r_s2 = get_res_near_ligand(s2, r_to_i_map_s2)
 
-							valid_r_s1 = get_res_near_ligand(s1)
-							valid_r_s2 = get_res_near_ligand(s2)
+					if valid_r_s1 == set({}):
+						print(protein, start, "no residues close to the ligand")
+						#continue
 
-							final_r_list_s1 = []
-							final_r_list_s2 = []
+					if valid_r_s1 == 0:
+						print(protein, target, "pose viewer file has no ligand")
+						#continue
 
-							for r in valid_r_s1:
-								s1index = r_to_i_map_s1[r]
-								if paired_str_s1[s1index] == paired_str_s2[s1index]:
-									if r not in final_r_list_s1:
-										final_r_list_s1.append(r)
-									if i_to_r_map_s2[s1index] not in final_r_list_s2:
-										final_r_list_s2.append(i_to_r_map_s2[s1index])
+					if valid_r_s2 == set({}):
+						print(protein, start, "no residues close to the ligand")
+						#continue
 
-							for r in valid_r_s2:
-								s2index = r_to_i_map_s2[r]
-								if paired_str_s2[s2index] == paired_str_s1[s2index]:
-									if r not in final_r_list_s2:
-										final_r_list_s2.append(r)
-									if i_to_r_map_s1[s2index] not in final_r_list_s1:
-										final_r_list_s1.append(i_to_r_map_s1[s2index])
+					if valid_r_s2 == 0:
+						print(protein, target, "pose viewer file has no ligand")
+						#continue
 
-							(asl_list_s1, a_list_s1) = get_atoms(s1, final_r_list_s1)
-							(asl_list_s2, a_list_s2) = get_atoms(s2, final_r_list_s2)
+					final_r_list_s1 = []
+					final_r_list_s2 = []
 
-							for k in range(len(a_list_s1)):
-								if len(a_list_s1[k]) == len(a_list_s2[k]):
-									rmsd_val = rmsd.calculate_in_place_rmsd(s1, a_list_s1[k], s2, a_list_s2[k])
-									feature = ASL_to_feature[protein][start][asl_list_s1[k]]
-									writer.writerow([protein, start, target, rmsd_val, feature[0], feature[1], feature[2], feature[3], feature[4]])
+					for r in valid_r_s1:
+						s1index = r_to_i_map_s1[r]
+						if paired_str_s1[s1index] == paired_str_s2[s1index]:
+							if r not in final_r_list_s1:
+								final_r_list_s1.append(r)
+							if i_to_r_map_s2[s1index] not in final_r_list_s2:
+								final_r_list_s2.append(i_to_r_map_s2[s1index])
 
+					for r in valid_r_s2:
+						s2index = r_to_i_map_s2[r]
+						if paired_str_s2[s2index] == paired_str_s1[s2index]:
+							if r not in final_r_list_s2:
+								final_r_list_s2.append(r)
+							if i_to_r_map_s1[s2index] not in final_r_list_s1:
+								final_r_list_s1.append(i_to_r_map_s1[s2index])
+
+					(asl_list_s1, a_list_s1) = get_atoms(s1, final_r_list_s1)
+					(asl_list_s2, a_list_s2) = get_atoms(s2, final_r_list_s2)
+
+					for k in range(len(a_list_s1)):
+						if len(a_list_s1[k]) == len(a_list_s2[k]):
+							rmsd_val = rmsd.calculate_in_place_rmsd(s1, a_list_s1[k], s2, a_list_s2[k])
+							feature = ASL_to_feature[protein][start][asl_list_s1[k]]
+							writer.writerow([protein, start, target, rmsd_val, feature[0], feature[1], feature[2], feature[3], feature[4]])
+
+
+if __name__ == '__main__':
+	task = sys.argv[1]
+	combind_root = '/scratch/PI/rondror/combind/bpp_data/'
+	result_folder = '/home/users/sidhikab/flexibility_project/flexibility_prediction/Data'
+	save_folder = result_folder+'/rmsds/'
+	partition = 'rondror'
+	infile = open('../Data/ASL_to_resinfo_dict', 'rb')
+	ASL_to_feature = pickle.load(infile)
+	infile.close()
+
+	if task == 'all':
+		proteins = get_proteins(combind_root)
+		#submit jobs for each protein
+		cmd = 'sbatch -p {} -t 1:00:00 -o {}_rmsd.out --wrap="$SCHRODINGER/run python3 rmsd_calculator.py  protein {}"'
+		for prot_name in proteins:
+			print(prot_name)
+			if not os.path.exists(save_folder + '{}_rmsds.csv'.format(prot_name)):
+				os.system(cmd.format(partition, save_folder + '/' + prot_name, prot_name))
+				time.sleep(0.5)
+			else:
+				print("Exists")
+
+	if task == 'protein':
+		protein = sys.argv[2]
+		rmsd_file = save_folder + '{}_rmsds.csv'.format(protein)
+		print(protein, rmsd_file)
+		compute_protein_rmsds(protein, rmsd_file, ASL_to_feature, combind_root)
