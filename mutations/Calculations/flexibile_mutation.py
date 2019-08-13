582f2552571090591e22506018384b64c57fad65
sidhikabalachandar <sidhikab@stanford.edu>
2019-08-13 12:51:16 -0700

    Merge remote-tracking branch 'remotes/origin/master' into sidhikaB_flexibility_prediction
    # Conflicts:
    #	flexibility_prediction/Analysis/flexibility_prediction_analysis.ipynb
    #	flexibility_prediction/Data/res_flexibility.csv

--- mutations/Calculations/flexibile_mutation.py
+++ mutations/Calculations/flexibile_mutation.py
@@ -32,16 +32,22 @@ if __name__ == '__main__':
         with structure.StructureWriter('{}/{}_to_{}.mae'.format(save_location, start, target)) as all:
             ending = '{}/structures/aligned_files/{}/{}_out.mae'.format(protein, start, start)
             s = list(structure.StructureReader(combind_root + ending))[0]
-            pair_data = data[data['start ligand'] == start & data['target ligand'] == target]
+            pair_data = data[(data['start ligand'] == start) & (data['target ligand'] == target)]
             counter = 0
             mutate_list = []
             for m in list(s.molecule):
                 if len(m.residue) != 1:
                     for r in list(m.residue):
+                        if counter >= len(pair_data):
+                            break
                         res_data = pair_data.iloc[counter]
-                        if list(r.atom)[0].chain == "A" and list(r.atom)[0].pdbcode == res_data['name'] and list(r.atom)[0].resnum == res_data['num']:
+                        if list(r.atom)[0].chain == "A" and r.pdbres == res_data['name'] and list(r.atom)[0].resnum == res_data['num'] and res_data['rmsd'] > 2:
                             print(list(r.atom)[0].pdbcode, list(r.atom)[0].resnum)
-                            atom_dict = build.mutate(s, list(r.atom)[0], 'ALA')
+                            mutate_list.append(list(r.atom)[0])
+                            counter += 1
+
+            for atom in mutate_list:
+                build.mutate(s, atom, 'ALA')
 
 
             all.append(s)
